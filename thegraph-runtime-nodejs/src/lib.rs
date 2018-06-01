f26b653f1d44f0c75ebecf6c692934f857280cfa
Jannis Pohlmann <contact@jannispohlmann.de>
2018-06-01 02:46:00 +0200

    Squashme: Runtime adapter

--- thegraph-runtime-nodejs/src/lib.rs
+++ thegraph-runtime-nodejs/src/lib.rs
@@ -1,8 +1,13 @@
+extern crate futures;
+extern crate http;
+extern crate hyper;
 #[macro_use]
 extern crate slog;
+extern crate tempdir;
 extern crate thegraph;
+extern crate tokio_core;
 
 mod adapter;
 mod server;
 
-pub use self::adapter::RuntimeAdapter;
+pub use self::adapter::{RuntimeAdapter, RuntimeAdapterConfig};
